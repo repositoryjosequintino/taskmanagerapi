5d668d71be0d039c88f6614780df6fc5447ebb79
José Quintinno <josequintino@hotmail.com.br>
2025-01-19 12:57:38 -0300

    feature: Configurar endpoint informativo da API

--- src/main/java/br/com/quintinno/taskmanagerapi/Application.java
+++ src/main/java/br/com/quintinno/taskmanagerapi/Application.java
@@ -2,12 +2,22 @@ package br.com.quintinno.taskmanagerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping
+	public String api() {
+		return "Welcome to Task Manager API on Port 8081!";
 	}
 
 }
